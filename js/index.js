f9882f2bd7bafc127a2d848db5b6b13568ad2af1
Artem <tonyakardeony@gmail.com>
2017-02-24 21:57:57 +0200

    With SVG

--- js/index.js
+++ js/index.js
@@ -13,10 +13,31 @@
             	$(div).show();
             }
         });
+        $(".indicators-list li ul i").on("click", function() {
+            var div = $(this).parent("li").children(".content");
+            if($(div).css("display") == "block") {
+                $(div).hide();
+            } else {
+                $(div).show();
+            }
+        });
+
+        $(".add-btn").on("click", function() {
+                $("#fields").hide();
+                $("#input").show();
+        });
 
         $(".search-category a").on("click", function() {
+            $(".indicators-list > ul > div").hide();
+            var classesNameOfCategoryItem = $(this).attr("class");
+            if(classesNameOfCategoryItem != undefined) {
+                var idOfBlockForShow = "#" + classesNameOfCategoryItem.replace("active", "").replace(".", "");
+            }
+            
+            $(idOfBlockForShow).show();
             $(this).parent("li").parent("ul").find("a").removeClass("active");
-            $(this).addClass("active")
+            $(this).addClass("active");
+            
         })
 
     });
